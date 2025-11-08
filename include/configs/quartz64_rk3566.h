0aa353d31c36772e9eb06c9441b9b95502d47739
Dang Huynh <dang.huynh@mainlining.org>
2025-11-08 12:38:05 +0700

    configs: quartz64: Enable vidconsole
    Now that we have VOP2 support, we should enable vidconsole.
    
    Signed-off-by: Dang Huynh <dang.huynh@mainlining.org>

--- include/configs/quartz64_rk3566.h
+++ include/configs/quartz64_rk3566.h
@@ -3,7 +3,10 @@
 #ifndef __QUARTZ64_RK3566_H
 #define __QUARTZ64_RK3566_H
 
-#define ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdin=serial,usbkbd\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
 
 #include <configs/rk3568_common.h>
 
